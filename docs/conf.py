408f6e296b0c1a8bed2d9bf34dc3a553193f8a0b
Kayce Basques <kayce@google.com>
2024-01-19 18:22:42 -0800

    Get the published extension working

--- docs/conf.py
+++ docs/conf.py
@@ -4,12 +4,12 @@ author = 'Kayce Basques'
 release = '0.1.0'
 
 
-extensions = ['sphinx_embeddings']
+extensions = ['sphinx-embeddings']
 
 
 templates_path = ['_templates']
-exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+exclude_patterns = ['_build', 'venv']
 
 
 html_theme = 'alabaster'
-html_static_path = ['_static']
+# html_static_path = ['_static']
