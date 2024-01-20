ffc3b0f1ee4a6ada6c85990629b025678c24079b
Kayce Basques <kayce@google.com>
2024-01-19 18:17:23 -0800

    Try to fix the package stuff

--- setup.py
+++ setup.py
@@ -6,8 +6,8 @@ from setuptools import setup, find_packages
 
 setup(
     name='sphinx-embeddings',
-    version='0.0.1',
-    packages=find_packages(),
+    version='0.0.2',
+    packages=['sphinx-embeddings'],
     # install_requires=install_requires(),
     install_requires=[],
     classifiers=[],
