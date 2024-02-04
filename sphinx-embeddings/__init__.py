495749d31fcd071752fdfc98678773ba8ae1eee4
Kayce Basques <kayce@google.com>
2024-02-03 18:15:02 -0800

    Delete old docs

--- sphinx-embeddings/__init__.py
+++ sphinx-embeddings/__init__.py
@@ -15,19 +15,28 @@ logger.addHandler(handler)  # debug
 logger.setLevel(logging.DEBUG)  # debug
 
 
+def prune_docs(actual_docs):
+    maybe_docs = data.keys()
+    old_docs = [doc for doc in maybe_docs if doc not in actual_docs]
+    for doc in old_docs:
+        del data[doc]
+
+
+def on_build_finished(app: Sphinx, exception) -> None:
+    prune_docs(app.env.found_docs)
+    with open(srcpath, 'w') as f:
+        json.dump(data, f, indent=4)
+    with open(outpath, 'w') as f:
+        json.dump(data, f, indent=4)
+
+
 def prune_sections(docname, before, after):
     old_data = [hash for hash in before if hash not in after]
     for hash in old_data:
         del data[docname][hash]
-    # TODO: Delete old docs?
 
 
 def embed(fn: Callable, text: str) -> List:
-    response = gemini.embed_content(
-        model='models/embedding-001',
-        content=text,
-        task_type='SEMANTIC_SIMILARITY'
-    )
     return fn(text)
 
 
@@ -50,13 +59,6 @@ def on_doctree_resolved(app: Sphinx, doctree: document, docname: str) -> None:
         embedding = app.config.sphinx_embeddings_function(text)
         data[docname][hash]['embedding'] = embedding
     prune_sections(docname, before, after)
-    
-
-def on_build_finished(app: Sphinx, exception) -> None:
-    with open(srcpath, 'w') as f:
-        json.dump(data, f, indent=4)
-    with open(outpath, 'w') as f:
-        json.dump(data, f, indent=4)
 
 
 def init_configs(app: Sphinx) -> None:
