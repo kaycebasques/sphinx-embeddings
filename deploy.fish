8c1fb0a4b89e9cf47465be11c6360475ec7a475e
Kayce Basques <kayce@google.com>
2024-12-29 08:38:09 -0800

    Convert deploy script to Fish

--- deploy.fish
+++ deploy.fish
@@ -1,7 +1,5 @@
-if [ -d dist ]; then
-    rm -rf dist
-fi
-source venv/bin/activate
+if test -d dist; rm -rf dist; end
+. venv/bin/activate.fish
 python3 -m pip install setuptools wheel twine
 python3 setup.py sdist bdist_wheel
 twine upload dist/*
